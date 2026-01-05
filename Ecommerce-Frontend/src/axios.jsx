cece7c774afae23832381df3690aaa05843659a0
kaustubhchile <72078555+kaustubhchile@users.noreply.github.com>
2026-01-05 14:20:03 +0530

    Modify axios instance for API usage
    Updated the axios instance to include API path and headers.

--- Ecommerce-Frontend/src/axios.jsx
+++ Ecommerce-Frontend/src/axios.jsx
@@ -1,8 +1,10 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "http://k8s-ecommerc-ecommerc-beadc8ac6d-1700331625.ap-south-1.elb.amazonaws.com"
+const API = axios.create({
+  baseURL: "http://k8s-ecommerc-ecommerc-beadc8ac6d-1700331625.ap-south-1.elb.amazonaws.com/api",
+  headers: {
+    "Content-Type": "application/json"
+  }
 });
 
-export default api;
-
+export default API;
