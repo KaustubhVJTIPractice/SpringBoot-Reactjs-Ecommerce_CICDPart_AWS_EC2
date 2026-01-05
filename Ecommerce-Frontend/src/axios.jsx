bd7cb1c4e779fbe60ce35dc702f5e0e25a0d11f8
kaustubhchile <72078555+kaustubhchile@users.noreply.github.com>
2026-01-05 12:21:34 +0530

    Update axios instance with new base URL

--- Ecommerce-Frontend/src/axios.jsx
+++ Ecommerce-Frontend/src/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API = axios.create({
-  baseURL: "/api",    // ALB routes this to backend
+const api = axios.create({
+  baseURL: "http://k8s-ecommerc-ecommerc-beadc8ac6d-1700331625.ap-south-1.elb.amazonaws.com"
 });
 
 export default API;
