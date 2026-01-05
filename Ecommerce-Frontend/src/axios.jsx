3fd0547ce037ee0a85db8fe36e678258df7ccce5
kaustubhchile <72078555+kaustubhchile@users.noreply.github.com>
2026-01-05 14:41:36 +0530

    Add Content-Type header to axios instance

--- Ecommerce-Frontend/src/axios.jsx
+++ Ecommerce-Frontend/src/axios.jsx
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: "http://k8s-ecommerc-ecommerc-beadc8ac6d-1700331625.ap-south-1.elb.amazonaws.com/api",
+  headers: {
+    "Content-Type": "application/json"
+  }
 });
 
 export default API;
